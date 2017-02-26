5f745d02af34d49c43b24a0587838ecf79d1dfae
radityad <raditya.danang@gmail.com>
2017-02-26 23:31:17 +0700

    FUCK FORGOT TO REMOVE TOKEN

--- Config.php
+++ Config.php
@@ -1,8 +1,6 @@
 <?php
-  //EDIT $WEBHOOK_URL with your Webhook URL from Discord
-	//https://hooks.slack.com/services/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
-//https://discordapp.com/api/webhooks/259413299672776704/PDmF4lQ7lH_J8q6ty2sB3tjowGtj0igUXacQ1gGzeO4vlhtanZr9YHsFa2UOLmL3sZa7
-  define('WEBHOOK_URL', 'https://hooks.slack.com/services/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');
+
+  define('WEBHOOK_URL', 'https://hooks.slack.com/services/T038RGMSP/B3N6XJG2E/***');
   define('API','https://a.4cdn.org');
   define('API_IMG' , 'https://i.4cdn.org');
   //define('ACTION', $_POST['action']);
